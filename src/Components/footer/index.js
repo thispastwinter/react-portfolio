fdf85573630d6c7e0519dc3287b31a72c27ac3af
thispastwinter <klaas.justin@gmail.com>
2019-09-18 11:06:55 -0500

    refined footer

--- src/Components/footer/index.js
+++ src/Components/footer/index.js
@@ -11,17 +11,17 @@ class Footer extends Component {
     }
     render() {
         return (
-            <div>
-                <div className="footer is-centered">
+            <div className="footer">
+                <div className="is-centered">
                 <NavBar array={routes} footer="true" />
                 </div>
-                <div className="footer is-centered">
+                <div className="is-centered">
                     <div className="grid-cell-sm is-spaced icons">
                         <FontAwesomeIcon icon={faGithub}></FontAwesomeIcon>
                         <FontAwesomeIcon icon={faLinkedin}></FontAwesomeIcon>
                     </div>
                 </div>
-                <div className="footer is-centered copyright">
+                <div className="is-centered copyright">
                 Copyright 2019 Justin Klaas
                 </div>
             </div>
